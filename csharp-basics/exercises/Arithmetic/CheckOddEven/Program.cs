fe7d57bb4b6ec1ed9fc7588a1b2a8775a4019db1
kSenfelds <kr.senfelds@gmail.com>
2023-02-16 15:59:53 +0200

    Exercise2 done

---
+++ csharp-basics/exercises/Arithmetic/CheckOddEven/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CheckOddEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int exit = 1;
+            while (exit > 0)
+            {
+                Console.WriteLine("Please enter a number or exit to exit the programm!");
+                var input = Console.ReadLine();
+                if (input == "exit")
+                {
+                    exit = 0;
+                }
+                else if (int.Parse(input) % 2 == 0)
+                {
+                    Console.WriteLine("Even Number");
+                }
+                
+                else
+                {
+                    Console.WriteLine("Odd Number");
+                }
+            }
+           
+            Console.WriteLine("bye!");
+
+        }
+    }
+}
