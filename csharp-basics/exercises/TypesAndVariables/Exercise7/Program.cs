d2d4e6bfc60f112ec54e4ce3db6898fe5e47b7e6
kSenfelds <kr.senfelds@gmail.com>
2023-02-16 11:45:59 +0200

    Exercise 7 done

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise7/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string with Uppercase letters and Lowercase letters!");
+            string input = Console.ReadLine();
+            int upperCaseCounter = 0;
+            for (int i=0; i<input.Length; i++)
+            {
+                if (Char.IsUpper(input[i]))
+                {
+                    upperCaseCounter++;
+                }
+            }
+            Console.WriteLine(upperCaseCounter);
+            
+        }
+    }
+}
