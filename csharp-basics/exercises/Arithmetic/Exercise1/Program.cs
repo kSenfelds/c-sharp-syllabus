d70aafeb3cc912f1ae139b9bd861a2cd46c981b6
kSenfelds <kr.senfelds@gmail.com>
2023-02-16 15:23:36 +0200

    Exercise1 done

---
+++ csharp-basics/exercises/Arithmetic/Exercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number 1");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter number 2");
+            int number2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(is15(number1, number2));
+        }
+        static bool is15 (int number1, int number2)
+        {
+            return number1 == 15 || number2 == 15 || number1 + number2 == 15 || number1 - number2 == 15;
+        }
+    }
+}
