740b2cfa046e6e2e126062cc5b4fef3115d0a38b
kSenfelds <kr.senfelds@gmail.com>
2023-02-16 11:52:50 +0200

    Exercise 6 done

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise6/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a single digit number!");
+            int numberA = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter a single digit number!");
+            int numberB = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter a single digit number!");
+            int numberC = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The sum of numbers that You have entered is {numberA + numberB + numberC}");
+        }
+    }
+}
