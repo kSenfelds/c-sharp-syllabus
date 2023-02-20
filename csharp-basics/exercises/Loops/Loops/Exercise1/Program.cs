029336c485cd121648cdec2345ecbdfcb716f67b
kSenfelds <kr.senfelds@gmail.com>
2023-02-20 12:16:32 +0200

    Exercise 1 done

--- csharp-basics/exercises/Loops/Loops/Exercise1/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise1/Program.cs
@@ -6,10 +6,13 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            int i;
-
+          
             Console.WriteLine("The first 10 natural numbers are: ");
-
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
             //todo - write a program in C# to display the first 10 natural numbers
 
         }
