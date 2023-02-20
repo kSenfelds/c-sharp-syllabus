3602f4f5a3016a74395706bac2dd1889eb1a0f12
kSenfelds <kr.senfelds@gmail.com>
2023-02-20 15:14:05 +0200

    Exercise 7 done

---
+++ csharp-basics/exercises/Loops/Loops/Exercise 7/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Piglet piglet= new Piglet();
+            piglet.gamePiglet();
+        }
+    }
+}
