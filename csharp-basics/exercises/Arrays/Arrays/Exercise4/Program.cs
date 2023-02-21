2b918ebe4084541a175aca71f17344d22ce663d1
kSenfelds <kr.senfelds@gmail.com>
2023-02-21 13:10:30 +0200

    Exercise 4 done

--- csharp-basics/exercises/Arrays/Arrays/Exercise4/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise4/Program.cs
@@ -8,7 +8,6 @@ namespace Exercise4
 {
     class Program
     {
-        //TODO: Write a C# program to test if an array contains a specific value.
         private static void Main(string[] args)
         {
             int[] myArray =
@@ -17,14 +16,17 @@ namespace Exercise4
                 1458, 2458, 1254, 1472, 2365,
                 1456, 2265, 1457, 2456
             };
-
-            /*
-            fixme - check if contains number 1245 
-            for (?) {
-                ......
-                Console.WriteLine("Contains!");
+            int checkValue = 1245;
+            string result = "Does not contain!";
+            for (int i=0; i<myArray.Length; i++) {
+                if (myArray[i] == checkValue)
+                {
+                   result = "Contains!";
+                }
             }
-            */
+            Console.WriteLine(result);
+            Console.ReadKey();
+            
         }
     }
 }
