06b3e8100cae73b51904274e01c12800ba368b46
kSenfelds <kr.senfelds@gmail.com>
2023-02-21 10:21:48 +0200

    ASCII Figure done

---
+++ csharp-basics/exercises/Loops/Loops/AsciiFigure/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AsciiFigure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           AsciiFigure ascii = new AsciiFigure();
+            ascii.printFigure(10);
+        }
+    }
+}
