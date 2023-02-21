5bf82aed17d7be5fc5e84b2754bc0486f065ab7f
kSenfelds <kr.senfelds@gmail.com>
2023-02-21 12:17:08 +0200

    Exercise 11 done

---
+++ csharp-basics/exercises/Loops/Loops/Exercise 11/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercise_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word for Reversecase :");
+            string input = Console.ReadLine();
+            ReverseCase(input);
+
+            static void ReverseCase (string sentence)
+            {
+                string[] chars = new string[sentence.Length];
+                for (int i=0; i< chars.Length; i++)
+                {
+                    if (char.IsLower(sentence[i]))
+                    {
+                        chars[i] = sentence[i].ToString().ToUpper();
+                    }
+                    else if (char.IsUpper(sentence[i]))
+                    {
+                        chars[i] = sentence[i].ToString().ToLower();
+                    }
+                    else
+                    {
+                        chars[i] = sentence[i].ToString();
+                    }
+                }
+                Console.WriteLine(String.Join("", chars));
+            }
+        }
+    }
+}
