febd60d6363f5351f0cf023e1a3f7160eadaa7fd
kSenfelds <kr.senfelds@gmail.com>
2023-02-25 12:20:28 +0200

    Exercise1 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise1/Product.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercise1
+{
+    internal class Product
+    {
+        private decimal _price;
+        private int _amount;
+        private string _name;
+
+        public Product(string name, decimal priceAtStart, int amountAtStart)
+        {
+            _name = name;
+            _price = priceAtStart;
+            _amount = amountAtStart;
+        }
+
+        public void PrintProduct()
+        {
+            Console.WriteLine($"{_name}, price {_price} EUR, amount {_amount} units.");
+        }
+
+        public int ChangeAmount(int newAmount)
+        {
+            _amount = newAmount;
+            return _amount;
+        }
+
+        public decimal ChangePrice(decimal newPrice)
+        {
+            _price = newPrice;
+            return _price;
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product logitechMouse = new Product("Logitech mouse", 70.00m, 14);
+            Product iPhone5 = new Product("iPhone 5s", 999.99m, 3);
+            Product epsonEb = new Product("Epson EB-U05", 440.46m, 1);
+
+            logitechMouse.PrintProduct();
+            iPhone5.PrintProduct();
+            epsonEb.PrintProduct();
+
+            logitechMouse.ChangeAmount(13);
+            logitechMouse.ChangePrice(75.00m);
+            logitechMouse.PrintProduct();
+        }
+    }
+}
