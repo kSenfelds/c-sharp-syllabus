287be52938c277939992c340d84f2a1128c0f171
kSenfelds <kr.senfelds@gmail.com>
2023-02-26 17:31:15 +0200

    Exercise4 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/Movie.cs
@@ -0,0 +1,37 @@
+﻿namespace Exercise4
+{
+    internal class Movie
+    {
+        private string _title;
+        private string _studio;
+        private string _rating;
+
+        public Movie(string title, string studio, string rating)
+        {
+            _title = title;
+            _studio = studio;
+            _rating = rating;
+        }
+
+        public Movie(string title, string studio)
+        {
+            _title = title;
+            _studio = studio;
+            _rating = "PG";
+        }
+
+        public static Movie[] GetPG(Movie[] movies)
+        {
+            Movie[] result = new Movie[movies.Length];
+            for (int i = 0; i < movies.Length; i++)
+            {
+                if (movies[i]._rating == "PG")
+                {
+                    result[i] = movies[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie zero = new Movie("Test title", "Test studio", "PG");
+            Movie one = new Movie("Casino Royale", "Eon Productions", "PG13");
+            Movie two = new Movie("Glass", "Buena Vista International", "PG13");
+            Movie three = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures");
+
+            Movie[] movies = { zero, one, two, three };
+            Movie[] pgMovies = Movie.GetPG(movies);
+            foreach (Movie movie in pgMovies)
+            {
+                Console.WriteLine(movie);
+            }
+        }
+    }
+}
