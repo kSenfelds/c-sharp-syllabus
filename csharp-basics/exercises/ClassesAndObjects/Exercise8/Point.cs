a76ad619ea0e22c4f194f8da03ef6ed807b567fe
kSenfelds <kr.senfelds@gmail.com>
2023-02-28 11:21:00 +0200

    Exercise 8 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise8/Point.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise8
+{
+    internal class Point
+    {
+        public int _x;
+        public int _y;
+
+        public Point(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public static void SwapPoint(Point point, Point otherPoint)
+        {
+            int x = point._x;
+            int y = point._y;
+            point._x = otherPoint._x;
+            point._y = otherPoint._y;
+            otherPoint._x = x;
+            otherPoint._y = y;
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise8/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(5, 2);
+            Point p2 = new Point(-3, 6);
+            Point.SwapPoint(p1, p2);
+            Console.WriteLine("(" + p1._x + ", " + p1._y + ")");
+            Console.WriteLine("(" + p2._x + ", " + p2._y + ")");
+        }
+    }
+}
