a0a71b65dfe474752eb113e3538a7d6a04af9f63
kSenfelds <kr.senfelds@gmail.com>
2023-02-28 20:59:32 +0200

    Exercise 11 done

--- csharp-basics/exercises/ClassesAndObjects/Account/Account.cs
+++ csharp-basics/exercises/ClassesAndObjects/Account/Account.cs
@@ -7,17 +7,18 @@
 
         public Account(string v1, double v2)
         {
-            
+            _name = v1;
+            _money = v2;
         }
 
         public double Withdrawal(double i)
         {
-            return i;
+            return _money -= i;
         }
 
         public void Deposit(double i)
         {
-            
+            _money += i;
         }
 
         public double Balance()
@@ -34,6 +35,12 @@
         {
             get => _name;
             set => _name = value;
+        }
+
+        public static void Transfer(Account from, Account to, double howMuch)
+        {
+            from.Withdrawal(howMuch);
+            to.Deposit(howMuch);
         }
     }
 }
--- csharp-basics/exercises/ClassesAndObjects/Account/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Account/Program.cs
@@ -10,6 +10,39 @@ namespace Account
     {
         private static void Main(string[] args)
         {
+            Account bartosAccount = new Account("Barto's account", 100.00);
+            Account bartosSwissAccount = new Account("Barto's account in Switzerland", 1000000.00);
+
+            Console.WriteLine("Initial state");
+            Console.WriteLine(bartosAccount);
+            Console.WriteLine(bartosSwissAccount);
+
+            bartosAccount.Withdrawal(20);
+            Console.WriteLine("Barto's account balance is now: " + bartosAccount.Balance());
+            bartosSwissAccount.Deposit(200);
+            Console.WriteLine("Barto's Swiss account balance is now: " + bartosSwissAccount.Balance());
+
+            Console.WriteLine("Final state");
+            Console.WriteLine(bartosAccount);
+            Console.WriteLine(bartosSwissAccount);
+
+            Account mattsAccount = new Account("Matt's account", 1000);
+            Account myAccount = new Account("My account", 0);
+            mattsAccount.Withdrawal(100);
+            myAccount.Deposit(100.0);
+            Console.WriteLine(mattsAccount);
+            Console.WriteLine(myAccount);
+
+            Account a = new Account("A", 100.0);
+            Account b = new Account("B", 0);
+            Account c = new Account("C", 0);
+            Account.Transfer(a, b, 50.0);
+            Account.Transfer(b, c, 25.0);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            Console.ReadKey();
         }
     }
 }
