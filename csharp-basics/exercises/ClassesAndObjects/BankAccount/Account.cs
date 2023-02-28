067fff2f0bb0fec4e7d1a0de661cb4b8c8d9aef1
kSenfelds <kr.senfelds@gmail.com>
2023-02-28 11:38:05 +0200

    Exercise 9 done

---
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/Account.cs
@@ -0,0 +1,19 @@
+﻿namespace BankAccount
+{
+    internal class Account
+    {
+        private string _name;
+        private decimal _balance;
+
+        public Account(string name, decimal balance)
+        {
+            _name = name;
+            _balance = balance;
+        }
+
+        public string ShowUserNameAndBalance()
+        {
+            return _balance > 0 ? $"{_name}, ${string.Format("{0:0.00}", _balance)}" : $"{_name}, -${string.Format("{0:0.00}", _balance * -1)}";
+        }
+    }
+}
--- csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BankAccount
 {
@@ -10,6 +6,15 @@ namespace BankAccount
     {
         static void Main(string[] args)
         {
+            Account benben = new Account("Benson", 17.50m);
+            Account kriss = new Account("Kriss", 100m);
+            Account lea = new Account("Lea", -17.63m);
+
+            Console.WriteLine(benben.ShowUserNameAndBalance());
+            Console.WriteLine(kriss.ShowUserNameAndBalance());
+            Console.WriteLine(lea.ShowUserNameAndBalance());
+
+            Console.ReadKey();
         }
     }
 }
