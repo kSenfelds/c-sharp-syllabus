a381e08537f19759927bc89a3fa0c56b8e964e65
kSenfelds <kr.senfelds@gmail.com>
2023-03-02 20:50:19 +0200

    Exercise 14 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise14/Date.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercise14
+{
+    internal class Date
+    {
+        public static void getNLday(int year, int month, int day)
+        {
+            var culture = new System.Globalization.CultureInfo("nl-NL");
+            DateTime time = new DateTime(year, month, day);
+            string weekday = culture.DateTimeFormat.GetDayName(time.DayOfWeek).ToString();
+            Console.WriteLine(weekday);
+        }
+    }
+}
