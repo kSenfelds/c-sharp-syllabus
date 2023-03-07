72148e1c269aa84400751c431eee89b9a8544622
kSenfelds <kr.senfelds@gmail.com>
2023-03-07 10:19:27 +0200

    Flight planner done

---
+++ csharp-basics/exercises/Collections/Exercise4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercise4
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool hasName = true;
+            List<string> names = new List<string>();
+            while (hasName)
+            {
+                Console.WriteLine("Enter name");
+                var input = Console.ReadLine();
+                names.Add(input);
+                if (input.Length == 0)
+                {
+                    hasName = false;
+                    var unique = names.Distinct().ToList();
+                    Console.WriteLine($"Unique name list contains: {string.Join(" ", unique)}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ csharp-basics/exercises/Collections/Exercise5/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Exercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            var input = int.Parse(Console.ReadLine());
+            var intStack = new Stack<int>();
+            intStack.Push(input);
+            do
+            {
+                var numbers = intStack.Pop().ToString().ToCharArray();
+                var result = 0d;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+
+                    result += Math.Pow(Convert.ToDouble(numbers[i].ToString()), 2d);
+                }
+                intStack.Push((int)result);
+            }
+            while (intStack.Peek() > 9);
+
+            if (intStack.Peek() == 1)
+            {
+                Console.WriteLine("happy");
+            }
+            else
+            {
+                Console.WriteLine("not happy");
+            }
+            Console.ReadKey();
+        }
+    }
+}
